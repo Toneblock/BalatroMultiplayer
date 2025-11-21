e06ab0d49a2438afb31adfcce9b9400924819feb
Stephen Kirk <me@thestephen.net>
2025-11-21 15:40:46 +0100

    the order is back

--- rulesets/sandbox.lua
+++ rulesets/sandbox.lua
@@ -118,7 +118,7 @@ MP.Ruleset({
 
 	force_lobby_options = function(self)
 		MP.LOBBY.config.preview_disabled = true
-		MP.LOBBY.config.the_order = false
+		MP.LOBBY.config.the_order = true
 		MP.LOBBY.config.starting_lives = 4
 		return true
 	end,
