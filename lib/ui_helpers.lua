060fa8c794ed631dec5e4b9ba66f3064ae378f6b
Stephen Kirk <me@thestephen.net>
2025-12-30 13:44:58 +0100

    done moving things around

--- lib/ui_helpers.lua
+++ lib/ui_helpers.lua
@@ -13,3 +13,18 @@ function MP.UTILS.get_from_clipboard()
 		return love.system.getClipboardText()
 	end
 end
+
+function MP.UTILS.random_message()
+	local messages = {
+		localize("k_message1"),
+		localize("k_message2"),
+		localize("k_message3"),
+		localize("k_message4"),
+		localize("k_message5"),
+		localize("k_message6"),
+		localize("k_message7"),
+		localize("k_message8"),
+		localize("k_message9"),
+	}
+	return messages[math.random(1, #messages)]
+end
